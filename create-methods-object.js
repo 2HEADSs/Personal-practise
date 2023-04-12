9833aee5f51484c919b1cdf0d9a2ecda505ed4bc
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-12 21:51:25 +0300

    add object as library

--- create-methods-object.js
+++ create-methods-object.js
@@ -1,4 +1,4 @@
- 
+
 let car = {
     model: 'BMW',
     year: '2015',
@@ -9,7 +9,7 @@ let car = {
     honk2: () => { //method with arrow function
         console.log('Beep beep again!');
     },
-    honk3(){  //Object method notation
+    honk3() {  //Object method notation
         console.log('Beep beep for last time!');
 
     }
@@ -18,3 +18,18 @@ let car = {
 car.honk();
 car.honk2();
 car.honk3();
+
+//Object as function library
+let calc = {
+    sum: function (a, b) {
+        return a + b
+    },
+    multiplication: function (a, b) {
+        return a * b
+    },
+    substraction: function (a, b) {
+        return a - b
+    },
+}
+
+console.log(calc.sum(10, 5));
