f963c74efbaeddeb5e2d8aa07f72958963ed4a2a
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-12 21:48:00 +0300

    add object notation demo

--- create-methods-object.js
+++ create-methods-object.js
@@ -1,11 +1,20 @@
-
+ 
 let car = {
     model: 'BMW',
     year: '2015',
     facelift: true,
-    honk: function () {
+    honk: function () { //method with function expression
         console.log('Beep beep');
+    },
+    honk2: () => { //method with arrow function
+        console.log('Beep beep again!');
+    },
+    honk3(){  //Object method notation
+        console.log('Beep beep for last time!');
+
     }
 }
 
-car.honk()
+car.honk();
+car.honk2();
+car.honk3();
