d3170c64b78abd74bbc7eea3a841bbadb8733207
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-23 22:58:16 +0300

    Create call-apply-bind.js

---
+++ advanced function/call-apply-bind.js
@@ -0,0 +1,11 @@
+//Call
+function introduce() {
+    console.log(`Hello my name is ${this.name}`);
+};
+introduce(); //global invokation - this.name ==undefined
+
+let personObject = {
+    name: 'Pesho'
+};
+
+introduce.call(personObject)
