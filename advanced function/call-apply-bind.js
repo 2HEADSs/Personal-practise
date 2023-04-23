9e9055f60c3124c7fd517d6a05f0799ab969c46b
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-23 23:14:53 +0300

    Update call-apply-bind.js

--- advanced function/call-apply-bind.js
+++ advanced function/call-apply-bind.js
@@ -1,12 +1,32 @@
 //Call
-function introduce() {
+function introduceShorter() {
     console.log(`Hello my name is ${this.name}`);
 };
-introduce(); //global invokation - this.name ==undefined
+introduceShorter(); //global invokation - this.name ==undefined
 
 let personObject = {
     name: 'Pesho'
 };
 
-introduce.call(personObject); //invoke using call;
-introduce.call({name: 'Gosho'}); //invoke using call and set this.name = 'Gosho';
+introduceShorter.call(personObject); //invoke using call;
+introduceShorter.call({ name: 'Gosho' }); //invoke using call and set this.name = 'Gosho';
+
+
+introduceShorter.apply(personObject); //invoke using apply;
+introduceShorter.apply({ name: 'Gosho' }); //invoke using apply and set this.name = 'Gosho';
+
+console.log('longer');
+
+
+function introduceLonger(firstName, lastName) {
+    console.log(`Hello ${firstName} ${lastName} my name is ${this.name}`);
+};
+
+introduceLonger('Stefan', 'Ivanov'); //global invokation - this.name ==undefined
+
+console.log('---------------------');
+introduceLonger.call(personObject, 'Stefan', 'Ivanov'); //invoke using call;
+introduceLonger.call({ name: 'Gosho' }, 'Stefan', 'Ivanov'); //invoke using call and set this.name = 'Gosho';
+
+introduceShorter.apply(personObject, ['Stefan', 'Ivanov']); //invoke using apply;
+introduceShorter.apply({ name: 'Gosho' }, ['Stefan', 'Ivanov' ]); //invoke using apply and set this.name = 'Gosho';
