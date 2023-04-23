262ef254f6d6805e3a74817ef0ed07557f794255
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-23 23:25:18 +0300

    Update call-apply-bind.js

--- advanced function/call-apply-bind.js
+++ advanced function/call-apply-bind.js
@@ -9,11 +9,11 @@ let personObject = {
 };
 
 introduceShorter.call(personObject); //invoke using call;
-introduceShorter.call({ name: 'Gosho' }); //invoke using call and set this.name = 'Gosho';
+introduceShorter.call({ name: 'Goran' }); //invoke using call and set this.name = 'Gosho';
 
 
 introduceShorter.apply(personObject); //invoke using apply;
-introduceShorter.apply({ name: 'Gosho' }); //invoke using apply and set this.name = 'Gosho';
+introduceShorter.apply({ name: 'Ivan' }); //invoke using apply and set this.name = 'Gosho';
 
 console.log('longer');
 
@@ -25,8 +25,20 @@ function introduceLonger(firstName, lastName) {
 introduceLonger('Stefan', 'Ivanov'); //global invokation - this.name ==undefined
 
 console.log('---------------------');
-introduceLonger.call(personObject, 'Stefan', 'Ivanov'); //invoke using call;
-introduceLonger.call({ name: 'Gosho' }, 'Stefan', 'Ivanov'); //invoke using call and set this.name = 'Gosho';
+introduceLonger.call(personObject, 'Purvan', 'Dragoev'); //invoke using call;
+introduceLonger.call({ name: 'Mihail' }, 'Sotir', 'Papazov'); //invoke using call and set this.name = 'Gosho';
 
-introduceShorter.apply(personObject, ['Stefan', 'Ivanov']); //invoke using apply;
-introduceShorter.apply({ name: 'Gosho' }, ['Stefan', 'Ivanov' ]); //invoke using apply and set this.name = 'Gosho';
+introduceShorter.apply(personObject, ['Drago', 'Rusinov']); //invoke using apply;
+introduceShorter.apply({ name: 'Niki' }, ['Antoniq', 'Chavdarova']); //invoke using apply and set this.name = 'Gosho';
+
+
+//Bind
+console.log('---BIND------');
+let supermen = {
+    name: 'Supermen'
+};
+
+introduceLonger.bind(supermen, 'Ivanka', 'Stoqnowa') //bind returns new function
+
+let superIntroduction = introduceLonger.bind(supermen, 'Lois', 'Lane') ;
+superIntroduction(); //log the result
