4f2c7efb1e25b1af757f529d26da9c9f590a5c2b
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-23 22:49:24 +0300

    Update arrow.js

--- advanced function/arrow.js
+++ advanced function/arrow.js
@@ -2,14 +2,24 @@ const person = {
     firstName: 'Pesho',
     lastName: 'Georgiev',
     introduce() {
+        console.log(this);//here this is person object
+
         // function getFullName - same as  const getFullName = function();
         const getFullName = function () {
             return this.firstName + ' ' + this.lastName
-        }
+        };
+
         console.log(`Hello, my name is ${getFullName()}`);
         //Hello, my name is undefined undefined - it is called without context:
         //getFullName() - just global invokation
-        console.log(`Hello, my name is ${getFullName()}`);
+
+        const getFullNameArrow = () => {
+            console.log(this);//here this is person object
+            return this.firstName + ' ' + this.lastName
+        };
+
+        console.log(`Hello, my name is ${getFullNameArrow()}`);
+        // Hello, my name is Pesho Georgiev - because arrow will use the uper context
 
     }
 }
