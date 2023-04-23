6ef946923c8d335c715d422f9627772751512153
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-23 22:41:48 +0300

    Update arrow.js

--- advanced function/arrow.js
+++ advanced function/arrow.js
@@ -7,6 +7,10 @@ const person = {
             return this.firstName + ' ' + this.lastName
         }
         console.log(`Hello, my name is ${getFullName()}`);
+        //Hello, my name is undefined undefined - it is called without context:
+        //getFullName() - just global invokation
+        console.log(`Hello, my name is ${getFullName()}`);
+
     }
 }
-person.introduce()
+person.introduce() //Hello, my name is undefined undefined - it is called without
