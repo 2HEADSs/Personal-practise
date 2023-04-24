e4a5c269ac1440eb0995e5ca6036c2ee5caa176f
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-24 22:35:08 +0300

    Update closure.js

--- closure.js
+++ closure.js
@@ -14,7 +14,7 @@ counter();
 counter();
 counter();
 counter();
-console.log('new closure');
+console.log('new closure ');
 counter2();
 //because fn inner keep the reference of the 
 //counter variable which is moved to the heap
