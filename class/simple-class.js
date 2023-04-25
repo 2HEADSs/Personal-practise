036e650bb36cf127a2260c591b4a2a14ee04ba91
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-25 16:51:57 +0300

    Update simple-class.js

--- class/simple-class.js
+++ class/simple-class.js
@@ -9,5 +9,7 @@ const bulgarian = new Human('male', 'Bulgarian');
 console.log(bulgarian);
 const latin = new Human('female', 'Latin');
 console.log(latin);
-latin.gender = 'male'
+latin.gender = 'male';
+console.log(latin);
+latin['language'] = 'Latin and Greece';
 console.log(latin);
