80d7b99241c403bb70b6f083b5195f932b2f04a1
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-25 16:49:56 +0300

    Update simple-class.js

---
+++ class/simple-class.js
@@ -0,0 +1,13 @@
+class Human {
+    constructor(gender, language) {
+        this.gender = gender,
+            this.language = language
+    }
+};
+
+const bulgarian = new Human('male', 'Bulgarian');
+console.log(bulgarian);
+const latin = new Human('female', 'Latin');
+console.log(latin);
+latin.gender = 'male'
+console.log(latin);
