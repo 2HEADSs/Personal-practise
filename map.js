39af6c5feda72ca29929c541a9eb7bb615a31a16
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-27 16:48:34 +0300

    Update map.js

--- map.js
+++ map.js
@@ -3,10 +3,20 @@ let demoMap = new Map();
 demoMap.set('name', 'Ivan');
 demoMap.set('age', 23);
 console.log(demoMap);
+console.log('---------------');
 
 console.log(demoMap.get('name'));
 console.log(demoMap.get('age'));
 
-
+console.log('---------------');
 demoMap.set('age', 21);
+console.log(demoMap);
+console.log('---------------');
+
+console.log(demoMap.keys());
+console.log(demoMap.values());
+console.log(demoMap.entries());
+console.log('---------------');
+
+demoMap.set(1, 'One');
 console.log(demoMap);
