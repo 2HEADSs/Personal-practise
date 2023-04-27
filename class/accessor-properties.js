343994e65826b414413ea3184c995c4adf64b795
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-27 12:53:44 +0300

    Update accessor-properties.js

--- class/accessor-properties.js
+++ class/accessor-properties.js
@@ -6,8 +6,19 @@ class Circle {
     get diameter() {
         return this.radius * 2;
     };
+
+    set diameter(diameter) {
+        return this.radius = diameter / 2
+    }
 };
 
 let firstCircle = new Circle(2);
 console.log(`Radius = ${firstCircle.radius}`);
 console.log(`Diameter = ${firstCircle.diameter}`);
+//set dimeter via set 
+firstCircle.diameter = 10;
+console.log(`Diameter = ${firstCircle.diameter}`);
+console.log(`Radius = ${firstCircle.radius}`);
+
+
+
