23b85cb360284e84a3024f8338ac846883d4390c
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-27 16:47:01 +0300

    Create map.js

---
+++ map.js
@@ -0,0 +1,12 @@
+let demoMap = new Map();
+
+demoMap.set('name', 'Ivan');
+demoMap.set('age', 23);
+console.log(demoMap);
+
+console.log(demoMap.get('name'));
+console.log(demoMap.get('age'));
+
+
+demoMap.set('age', 21);
+console.log(demoMap);
