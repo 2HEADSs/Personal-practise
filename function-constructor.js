0af67b85e1463ff0e2e286dc6b9b777c780eae1b
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-30 13:55:33 +0300

    add method to function prororype

--- function-constructor.js
+++ function-constructor.js
@@ -2,7 +2,20 @@ function Person(name, age, height) {
     this.name = name;
     this.age = age;
     this.height = height;
+    //This will create function for each instance
+    // this.greed = function () {
+    //     console.log(`Hello! My name is ${this.name}`);
+    // };
 };
 
-let firstPerson = new Person('Pesho', 23, 178);
+let firstPerson = new Person('Pesho', 23, 169);
+let secondPerson = new Person('Gosho', 31, 177);
+
 console.log(firstPerson);
+
+Person.prototype.greed = function () {
+    console.log(`Hello! My name is ${this.name} and I'm ${this.age} years old.`);
+}
+
+firstPerson.greed();
+secondPerson.greed();
