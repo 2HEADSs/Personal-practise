6dfcd03caf7f9cfd300901b7cdb2f3dd95a0c446
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-30 10:28:20 +0300

    update demo

--- class/internal-properties.js
+++ class/internal-properties.js
@@ -4,3 +4,16 @@ let person = {
 };
 // console.log(Object.getOwnPropertyDescriptors(person));
 let internalNamePrperies = Object.getOwnPropertyDescriptor(person, 'name');
+
+//hide and also can not be itterate
+Object.defineProperty(person, 'name', { enumerable: false });
+console.log(person); //{age:20}
+
+//add new property
+Object.defineProperty(person, 'height', { value: 190, enumerable: false });
+console.log(person);
+
+for (const key in person) {
+    console.log(key);
+    //age- only enumarable property
+}
