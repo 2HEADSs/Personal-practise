4c9a20fa805b07c969eafcf515da087f2c61fe0e
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-30 15:33:52 +0300

    Update prototype-inheritance.js

--- prototype-inheritance.js
+++ prototype-inheritance.js
@@ -12,5 +12,8 @@ let cat = new Animal('Cat', 'Garry', 4);
 console.log(cat);
 
 function Cat(name, age) {
-    Animal.call(this,)
+    Animal.call(this, 'Cat', name, age);
 }
+
+//the prototype of cat object will have protype which is prototype of Animal
+Cat.prototype = Object.create(Animal.prototype)
