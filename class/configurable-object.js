f9c9d85d57672daf860abf0ea06de82e9656d0d7
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-30 11:32:25 +0300

    Update configurable-object.js

--- class/configurable-object.js
+++ class/configurable-object.js
@@ -1,4 +1,24 @@
+// 'use strict'
+
 let person = {
     name: "Pesho",
     age: 20
 };
+
+let personCar = {
+    brand: 'Oper',
+    model: 'Astra'
+}
+let clonedPerson = { ...person };
+let clonedPerson2 = Object.assign({}, person, personCar);
+//{ name: 'Pesho', age: 20, brand: 'Oper', model: 'Astra' };
+
+console.log(clonedPerson2);
+
+Object.freeze(person);
+person.weight = 80;
+person.name = 'Gosho'
+
+console.log(person);
+console.log(Object.getOwnPropertyDescriptors(person));
+
