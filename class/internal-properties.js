d2e29e4fb2b3feb2557765e5057cf7048552fceb
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-04-30 10:15:02 +0300

    Update internal-properties.js

--- class/internal-properties.js
+++ class/internal-properties.js
@@ -2,4 +2,5 @@ let person = {
     name: "Pesho",
     age: 20
 };
-console.log(Object.getOwnPropertyDescriptors(person));
+// console.log(Object.getOwnPropertyDescriptors(person));
+console.log(Object.getOwnPropertyDescriptor(person, 'name'));
