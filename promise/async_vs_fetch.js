d9e6c717ae997c177dc3d22b385d132bf162f764
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-05-16 17:01:41 +0300

    Create async_vs_fetch.js

---
+++ promise/async_vs_fetch.js
@@ -0,0 +1,16 @@
+function execute(x, sec) {
+    return new Promise(resolve => {
+        console.log('Start: ' + x);
+        setTimeout(() => {
+            console.log('End: ' + x);
+            resolve(x);
+        }, sec * 1000);
+    });
+};
+async function serialFlow() {
+    let result1 = await execute(1, 1);
+    let result2 = await execute(2, 2);
+    let result3 = await execute(3, 3);
+    let finalResult = result1 + result2 + result3;
+    console.log(finalResult);
+};
