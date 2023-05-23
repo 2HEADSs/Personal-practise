29910c16d3c8366e573b098df8cfd6ed222b07a1
Pavel.Dimitrov <2heads.pd@gmail.com>
2023-05-23 12:59:57 +0300

    Update async_vs_fetch.js

--- promise/async_vs_fetch.js
+++ promise/async_vs_fetch.js
@@ -19,13 +19,13 @@ serialFlow();
 
 console.log('------------');
 
-async function parallelFlow() {
-    let result1 = execute(1, 1);
-    let result2 = execute(2, 2);
-    let result3 = execute(3, 3);
-    let finalResult = await result1 + await result2 +
-        await result3;
-    console.log('parallelFlow ' + finalResult);
-};
+// async function parallelFlow() {
+//     let result1 = execute(1, 1);
+//     let result2 = execute(2, 2);
+//     let result3 = execute(3, 3);
+//     let finalResult = await result1 + await result2 +
+//         await result3;
+//     console.log('parallelFlow ' + finalResult);
+// };
 
-parallelFlow();
+// parallelFlow();
