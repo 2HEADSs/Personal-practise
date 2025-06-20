a3e27f09ba3b5888e18c90dba9bd9f9e45ddbde3
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-06-20 23:06:41 +0300

    Update 2620.Counter.js

--- leetcode/30_Days_of_JavaScript/2620.Counter.js
+++ leetcode/30_Days_of_JavaScript/2620.Counter.js
@@ -1,3 +1,6 @@
+// DECISION
+
+
 /**
  * @param {number} n
  * @return {Function} counter
@@ -14,6 +17,11 @@ var createCounter = function (n) {
  * counter() // 11
  * counter() // 12
  */
+
+
+
+//  CONDITION
+
 /*
 Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
 
