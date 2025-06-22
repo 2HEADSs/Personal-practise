c8279ca0701b123fe42f2735a2070caa7502617e
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-06-22 16:13:42 +0300

    format 2704.ToBeOrNotToBe.js

--- leetcode/30_Days_of_JavaScript/2704.ToBeOrNotToBe.js
+++ leetcode/30_Days_of_JavaScript/2704.ToBeOrNotToBe.js
@@ -1,32 +1,30 @@
 // DECISION
 
 var expect = function (val) {
-    return {
-        toBe(param) {
-            if (val !== param) {
-                throw new Error("Not Equal");
-            }
-            return true;
-        },
-        notToBe(param) {
-            if (val === param) {
-                throw new Error("Equal");
-            }
-            return true;
-        }
-    };
+  return {
+    toBe(param) {
+      if (val !== param) {
+        throw new Error("Not Equal");
+      }
+      return true;
+    },
+    notToBe(param) {
+      if (val === param) {
+        throw new Error("Equal");
+      }
+      return true;
+    },
+  };
 };
 
-console.log({ value: expect(5).toBe(5) });            // { value: true }
-console.log({ value: expect(5).notToBe(null) });      // { value: true }
+console.log({ value: expect(5).toBe(5) }); // { value: true }
+console.log({ value: expect(5).notToBe(null) }); // { value: true }
 
 try {
-    expect(5).toBe(null);
+  expect(5).toBe(null);
 } catch (e) {
-    console.log({ error: e.message });                // { error: "Not Equal" }
+  console.log({ error: e.message }); // { error: "Not Equal" }
 }
-
-
 
 //  CONDITION
 
@@ -36,10 +34,13 @@ try {
  */
 
 /*
-Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
+Write a function expect that helps developers test their code. 
+It should take in any value val and return an object with the following two functions.
 
-toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
-notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal". 
+toBe(val) accepts another value and returns true if the two values === each other. 
+If they are not equal, it should throw an error "Not Equal".
+notToBe(val) accepts another value and returns true if the two values !== each other.
+If they are equal, it should throw an error "Equal". 
 
 Example 1:
 
@@ -57,4 +58,3 @@ Input: func = () => expect(5).notToBe(null)
 Output: {"value": true}
 Explanation: 5 !== null so this expression returns true.
 */
-
