24224b066f86b6b87dbc0452dfd4f443a205aca8
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-07-03 12:18:27 +0300

    Create 2715.TimeoutCancellation.js

---
+++ leetcode/30_Days_of_JavaScript/2715.TimeoutCancellation.js
@@ -0,0 +1,65 @@
+// DECISION
+
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function (fn, args, t) {
+  const timerId = setTimeout(() => fn(...args), t);
+  let cancelFn = () => {
+    clearTimeout(timerId);
+  };
+  return cancelFn;
+};
+
+const result = [];
+
+const fn = (x) => x * 5;
+const args = [2],
+  t = 20,
+  cancelTimeMs = 50;
+
+const start = performance.now();
+
+const log = (...argsArr) => {
+  const diff = Math.floor(performance.now() - start);
+  result.push({ time: diff, returned: fn(...argsArr) });
+};
+
+const cancel = cancellable(log, args, t);
+
+const maxT = Math.max(t, cancelTimeMs);
+
+setTimeout(cancel, cancelTimeMs);
+
+setTimeout(() => {
+  console.log(result); // [{"time":20,"returned":10}]
+}, maxT + 15);
+
+//  CONDITION
+
+/*
+Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
+
+Note that minor deviation from millis in the actual sleep duration is acceptable.
+
+ 
+
+Example 1:
+
+Input: millis = 100
+Output: 100
+Explanation: It should return a promise that resolves after 100ms.
+let t = Date.now();
+sleep(100).then(() => {
+  console.log(Date.now() - t); // 100
+});
+Example 2:
+
+Input: millis = 200
+Output: 200
+Explanation: It should return a promise that resolves after 200ms.
+ 
+*/
