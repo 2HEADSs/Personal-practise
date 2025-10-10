d831895a2454b0bc8e5fc19ca77f860492ec7df1
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-10-10 23:09:24 +0300

    Update 2677. Chunk Array.js

--- leetcode/jsByDifficulty/2677. Chunk Array.js
+++ leetcode/jsByDifficulty/2677. Chunk Array.js
@@ -36,10 +36,11 @@ var chunk = function (arr, size) {
   for (let i = 0; i < arr.length; i += size) {
     result.push(arr.slice(i, i + size));
   }
+  console.log(result);
   return result;
 };
 
-chunk([1, 2, 3, 4, 5], 1);
+chunk([], 1);
 
 /*
 Given an array arr and a chunk size size, return a chunked array.
