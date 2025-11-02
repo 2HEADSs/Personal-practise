5c6be57564ae555f8cf0bab90feb64382c76febd
Pavel.Dimitrov <2heads.pd@gmail.com>
2025-11-02 11:56:41 +0200

    Update Snail Traversal.js

--- leetcode/jsByDifficulty/Snail Traversal.js
+++ leetcode/jsByDifficulty/Snail Traversal.js
@@ -3,10 +3,29 @@
  * @param {number} colsCount
  * @return {Array<Array<number>>}
  */
-Array.prototype.snail = function (rowsCount, colsCount) {};
+Array.prototype.snail = function (rowsCount, colsCount) {
+  let length = this.length;
+  if (rowsCount * colsCount !== length) {
+    return [];
+  }
+  let result = Array.from({ length: rowsCount }, () => []);
 
-
-
+  let index = 0;
+  for (let c = 0; c < colsCount; c++) {
+    if (c % 2 == 0) {
+      for (let r = 0; r < rowsCount; r++) {
+        result[r].push(this[index]);
+        index++;
+      }
+    } else {
+      for (let r = rowsCount - 1; r >= 0; r--) {
+        result[r].push(this[index]);
+        index++;
+      }
+    }
+  }
+  return result
+};
 
 const arr = [1, 2, 3, 4];
 arr.snail(1, 4); // [[1,2,3,4]]
